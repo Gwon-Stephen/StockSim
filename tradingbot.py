5589009c25a6e1f6bc4a7f64dd86f2ba206b609f
Stephen Gwon <gwonstephen@gmail.com>
2024-07-18 15:57:14 -0400

    Sentiment Model

--- tradingbot.py
+++ tradingbot.py
@@ -3,7 +3,9 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy 
 from lumibot.traders import Trader
 from datetime import datetime
-
+from alpaca_trade_api import REST
+from timedelta import Timedelta
+from finbert_utils import estimate_sentiment
 
 API_KEY = "PKRGWTLLWNM1HBD3TFNI"
 API_SECRET = "M8SyWEicQJ73EgqcruaAYbPaXeoVuEHezol3tyZw"
@@ -21,12 +23,26 @@ class MLTrader(Strategy):
         self.sleeptime = "24H"
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
     def position_sizing(self):
         cash = self.get_cash()
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash * self.cash_at_risk / last_price,0)
         return cash, last_price, quantity
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days.strftime('%Y-%m-%d')
+    
+    def get_sentiment(self):
+        today, three_days_earlier = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, start=three_days_earlier, end=today)
+
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment
 
     #every tick
     def on_trading_iteration(self):
@@ -34,6 +50,8 @@ class MLTrader(Strategy):
 
         if cash > last_price:
             if self.last_trade == None:
+                probability, sentiment = self.get_sentiment()
+                print(news)
                 order = self.create_order(
                     self.symbol,
                     quantity,
